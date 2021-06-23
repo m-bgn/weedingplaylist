32f4153753fc9a082001e0296b2b89e6522b623e
Marc-Antoine Begin <m.begin@outlook.fr>
2021-06-23 18:05:01 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root 'song#new'
+  root 'songs#new'
   # resources :playlists, only: [:show]
   resources :songs, only: [:new, :create]
-  get "home", to: "pages#home"
+  # get "home", to: "pages#home"
   get "playlist", to: "playlists#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
