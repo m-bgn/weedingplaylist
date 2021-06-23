6b45c13a86c296148ded7ed7c34b3e3c96abb16d
Marc-Antoine Begin <m.begin@outlook.fr>
2021-06-23 17:57:47 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'songs#new'
+  root 'song#new'
   # resources :playlists, only: [:show]
   resources :songs, only: [:new, :create]
   get "home", to: "pages#home"
